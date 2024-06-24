06f8a6a09423cc770324b2ad344b6eebe3e708df
pomegranata <imeldamaretta.putri346@gmail.com>
2024-06-24 18:22:24 +0700

    fix

---
+++ src/main/java/exception/OperatorList.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package exception;
+
+import java.util.*;
+
+/**
+ *
+ * @author Imelda Maretta Putri
+ * F11.2022.00053
+ */
+
+public class OperatorList {
+    public static void main(String[] args) {
+        List<String> operator = Arrays.asList("Hellagur", "Mlynar", "Totter", "Hoederer", "Stormeye", "Sharp");
+            
+            try {
+                searchOperator(operator, "SilverAsh");
+            } catch (OperatorUnavailable e) {
+                System.out.println("Operator not found: " + e.getMessage());
+            }
+    }
+
+    public static void searchOperator(List<String> operator, String operatorName) throws OperatorUnavailable {
+        try {
+            if (operator.contains(operatorName)) {
+                System.out.println("Operator found: " + operatorName);
+            } else {
+                throw new OperatorUnavailable(operatorName);
+            }
+        } catch (OperatorUnavailable e) {
+            System.out.println("Trying to find operator: " + operatorName);
+            throw e;
+        }
+    }
+}
+
+class OperatorUnavailable extends Exception{
+    public OperatorUnavailable(String operatorName) {
+        super(operatorName + " is unavailable");
+    }
+}
